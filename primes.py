332113c8757dd9cde6e6b207713ac9abe533fc66
Mohammed Fohpa <mfohpa@gmail.com>
2023-10-04 10:42:46 +0100

    add exception for list values < 1

--- primes.py
+++ primes.py
@@ -3,4 +3,19 @@
 
 def primes(number_of_primes):
     list = []
+    number = 2
+    
+    if number_of_primes < 1:
+        raise ValueError("The size of the list must be at least 1")
+        
+    while len(list) != number_of_primes:
+        isPrime = True
+        for i in range(2, number):
+            if number % i == 0 and number != 2:
+                isPrime = False
+                break
+        if isPrime:
+            list.append(number)
+        number += 1
+            
     return list
